116942320a9062cba7470e4909a211f242e1afe6
Md Ehtesham <mdehtesham313@gmail.com>
2025-08-09 23:07:40 +0530

    hero section modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,65 @@ export default function Home() {
     <main>
       <header>
         <div className="container">
-          <h1>RideNow Rentals</h1>
-          <nav>
-            <a href="#">Home</a>
-            <a href="#">Vehicles</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-          </nav>
+          <img src="/Rovio.svg" alt="brand-logo" width="100" height="100" />
+          <ul>
+            <li className="ctaButtons">
+              <a href="#"> Home </a>
+              <a href="#"> Vehicles </a>
+              <a href="#"> About </a>
+              <a href="#"> Contact us </a>
+            </li>
+          </ul>
         </div>
       </header>
 
       <section className="hero">
-        <h2>Find Your Ride, Own the Road</h2>
-        <p>Rent top-notch cars & bikes near you. Fast, affordable & hassle-free.</p>
-        <button>Browse Vehicles</button>
+        <div className="heroImage">
+          <img src="/hero-image.png" alt="Car" />
+        </div>
+        <div className="info">
+          <h2>Find Your Ride, Own the Road</h2>
+          <p>Rent top-notch cars & bikes near you. Fast, affordable & hassle-free.</p>
+          <button>Browse Vehicles</button>
+        </div>
       </section>
+      <div className="brand-logos">
+        <div className="logos-track">
+          <div className="logo-set">
+            <img src="/honda.svg" alt="honda" />
+            <img src="/hyundai.svg" alt="hyundai" />
+            <img src="/mahindra.svg" alt="mahindra" />
+            <img src="/suzuki.svg" alt="suzuki" />
+            <img src="/volvo.svg" alt="volvo" />
+            <img src="/yamahamotorcorporation.svg" alt="yamaha" />
+            <img src="/bmw.svg" alt="bmw" />
+            <img src="/bentley.svg" alt="bentley" />
+            <img src="/ford.svg" alt="ford" />
+            <img src="/ferrari.svg" alt="ferrari" />
+            <img src="/mazda.svg" alt="mazda" />
+            <img src="/tata.svg" alt="tata" />
+            <img src="/tesla.svg" alt="tesla" />
+            <img src="/vespa.svg" alt="vespa" />
+            <img src="/mg.svg" alt="mg" />
+            {/* DUPLICATE SET (important for a seamless loop) */}
+            <img src="/honda.svg" alt="honda" />
+            <img src="/hyundai.svg" alt="hyundai" />
+            <img src="/mahindra.svg" alt="mahindra" />
+            <img src="/suzuki.svg" alt="suzuki" />
+            <img src="/volvo.svg" alt="volvo" />
+            <img src="/yamahamotorcorporation.svg" alt="yamaha" />
+            <img src="/bmw.svg" alt="bmw" />
+            <img src="/bentley.svg" alt="bentley" />
+            <img src="/ford.svg" alt="ford" />
+            <img src="/ferrari.svg" alt="ferrari" />
+            <img src="/mazda.svg" alt="mazda" />
+            <img src="/tata.svg" alt="tata" />
+            <img src="/tesla.svg" alt="tesla" />
+            <img src="/vespa.svg" alt="vespa" />
+            <img src="/mg.svg" alt="mg" />
+          </div>
+        </div>
+      </div>
 
       <section className="features">
         <div className="feature-card">
